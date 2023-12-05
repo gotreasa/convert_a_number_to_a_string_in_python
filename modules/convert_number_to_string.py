984d36ed606b1b91d96b7ac159b407f5b2beef4f
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-05 13:21:29 +0000

    feat: should get '123' when the input is 123

--- modules/convert_number_to_string.py
+++ modules/convert_number_to_string.py
@@ -1,2 +1,4 @@
 def number_to_string(value: int):
+    if value == 123:
+        return "123"
     raise ValueError("Input must be a number")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -3,8 +3,12 @@ from modules import convert_number_to_string
 
 
 def describe_dummy_kata():
-    def should_eror_when_not_integer(capsys):
+    def should_eror_when_not_integer():
         """🧪 should show an error when receive something other than an integer"""
 
         with pytest.raises(ValueError, match="Input must be a number"):
             convert_number_to_string.number_to_string("not a number")
+
+    def should_return_string_of_123():
+        """🧪 should get '123' when the input is 123"""
+        assert convert_number_to_string.number_to_string(123) == "123"
