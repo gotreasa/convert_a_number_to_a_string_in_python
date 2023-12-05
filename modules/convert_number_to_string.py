3c87f6cd3ba21691f5148bffe397142643024c1f
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-05 13:27:11 +0000

    refactor: clearly specify that the input must be an integer

--- modules/convert_number_to_string.py
+++ modules/convert_number_to_string.py
@@ -1,4 +1,4 @@
 def number_to_string(value: int):
     if not isinstance(value, int):
-        raise ValueError("Input must be a number")
+        raise ValueError("Input must be an integer")
     return str(value)
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -6,8 +6,8 @@ def describe_dummy_kata():
     def should_eror_when_not_integer():
         """🧪 should show an error when receive something other than an integer"""
 
-        with pytest.raises(ValueError, match="Input must be a number"):
-            convert_number_to_string.number_to_string("not a number")
+        with pytest.raises(ValueError, match="Input must be an integer"):
+            convert_number_to_string.number_to_string("not an integer")
 
     def should_return_string_of_123():
         """🧪 should get '123' when the input is 123"""
